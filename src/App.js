ac23efdc44732b36c0351373d30cabdd156ba2ef
abdelli <abdellirania1@gmail.com>
2023-05-25 21:32:41 +0200

    envoi cloud

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Car from './Car';
+import MapContainer from './MapContainer';
 function App() {
  
   return (
     <div className="App">
+            <MapContainer/>
+
       <Car></Car>
     </div>
   );
